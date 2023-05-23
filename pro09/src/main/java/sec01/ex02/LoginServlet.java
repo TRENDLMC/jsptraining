1d4a5067b7e6b652b748575757f6139898225938
DESKTOP-SF6TVD3\503 <trend7936@naver.com>
2023-05-23 09:16:32 +0900

    쿠키와섹션1

---
+++ pro09/src/main/java/sec01/ex02/LoginServlet.java
@@ -0,0 +1,50 @@
+package sec01.ex02;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URLEncoder;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init  메서드 호출");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out=response.getWriter();
+		String user_id =request.getParameter("user_id");
+		String user_pwd =request.getParameter("user_pwd");
+		String user_address=request.getParameter("user_address");
+		String user_email=request.getParameter("user_email");
+		String user_hp=request.getParameter("user_hp");
+		String data="안녕하세요!<br>로그인하셧습니다<br><br>";
+		data+="<html><body>";
+		data+="아이디:"+user_id;
+		data+="<br>";
+		data+="비밀번호:"+user_pwd;
+		data+="<br>";
+		data+="조소:"+user_address;
+		data+="<br>";
+		data+="email:"+user_email;
+		data+="<br>";
+		data+="전화번호:"+user_hp;
+		out.print(data);
+		
+		user_address=URLEncoder.encode(user_address,"utf-8");
+		out.print("<br><a href='/pro09/second?user_id="+user_id+"&user_pwd="+user_pwd+"&user_address="+user_address+"'>두번쨰 서블렛으로 보내기 </a>");
+		data="</body></html>";
+		out.print(data);
+	}
+
+}
